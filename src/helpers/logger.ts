a90caf53eafc16f3bbea20204e5c246173670bb5
Akaza Renn <8340896+AkazaRenn@users.noreply.github.com>
2025-02-20 15:41:38 -0500

    Remove timestamp from logger because DevTools support it natively

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -9,27 +9,23 @@ import DeckyLogger from "../deps/decky_logger";
 
 class Logger extends DeckyLogger {
     async debug(...args: any[]) {
-        super.debug(this.now(), ...args);
+        super.debug(...args);
         log_debug(args.join(' '));
     }
 
     async info(...args: any[]) {
-        super.log(this.now(), ...args);
+        super.log(...args);
         log_info(args.join(' '));
     }
 
     async error(...args: any[]) {
-        super.error(this.now(), ...args);
+        super.error(...args);
         log_error(args.join(' '));
     }
 
     async warning(...args: any[]) {
-        super.warn(this.now(), ...args);
+        super.warn(...args);
         log_warning(args.join(' '));
-    }
-
-    private now(): string {
-        return (new Date).toISOString();
     }
 }
 
